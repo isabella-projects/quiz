a5d821a031ef7f700723f505281fd1bd2ecf71c7
isabella-projects <damyan4oy@gmail.com>
2024-02-25 21:52:51 +0100

    Finishing quiz plugin

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import './index.scss';
-import { TextControl, Flex, FlexBlock, FlexItem, Button, Icon } from '@wordpress/components';
+import { TextControl, Flex, FlexBlock, FlexItem, Button, Icon, PanelBody, PanelRow, ColorPicker } from '@wordpress/components';
+import { InspectorControls, BlockControls, AlignmentToolbar } from '@wordpress/block-editor';
 
 (function () {
     let locked = false;
@@ -30,6 +31,18 @@ wp.blocks.registerBlockType('multiple-choice/quiz', {
         question: { type: 'string' },
         answers: { type: 'array', default: [''] },
         correctAnswer: { type: 'number', default: undefined },
+        bgColor: { type: 'string', default: '#EBEBEB' },
+        theAlignment: { type: 'string', default: 'left' },
+    },
+    description: 'Give your audience a chance to prove their comprehension.',
+    example: {
+        attributes: {
+            question: 'When was John Lennon born?',
+            correctAnswer: 3,
+            answers: ['1937', '1935', '1940', '1943'],
+            theAlignment: 'center',
+            bgColor: '#CFE8F1',
+        },
     },
     edit: EditComponent,
     save: function (props) {
@@ -56,7 +69,18 @@ function EditComponent(props) {
     }
 
     return (
-        <div className="quiz-edit-block">
+        <div className="quiz-edit-block" style={{ backgroundColor: props.attributes.bgColor }}>
+            <BlockControls>
+                <AlignmentToolbar value={props.attributes.theAlignment} onChange={(alignType) => props.setAttributes({ theAlignment: alignType })} />
+            </BlockControls>
+            <InspectorControls>
+                <PanelBody title="Background Color" initialOpen={true}>
+                    <ColorPicker
+                        color={props.attributes.bgColor}
+                        onChangeComplete={(pickerType) => props.setAttributes({ bgColor: pickerType.hex })}
+                    />
+                </PanelBody>
+            </InspectorControls>
             <TextControl
                 label="Question:"
                 value={props.attributes.question}
@@ -87,6 +111,17 @@ function EditComponent(props) {
                         </FlexItem>
                         <FlexItem>
                             <Button variant="link" className="attention-delete" onClick={() => deleteAnswers(index)}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    class="bi bi-trash"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                                </svg>
                                 Delete
                             </Button>
                         </FlexItem>
